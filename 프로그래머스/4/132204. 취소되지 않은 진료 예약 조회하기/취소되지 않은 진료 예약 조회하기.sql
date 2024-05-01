7d45ad5bc7dabad4c17318547c19440d735aab33
ydh5017 <64765991+ydh5017@users.noreply.github.com>
2024-05-01 19:57:27 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+# SELECT
+# from 
+
+select a.APNT_NO, p.PT_NAME, a.PT_NO, a.MCDP_CD, d.DR_NAME, a.APNT_YMD
+from APPOINTMENT a
+join PATIENT p on a.PT_NO = p.PT_NO
+join DOCTOR d on a.MDDR_ID = d.DR_ID
+where a.APNT_YMD like '2022-04-13%'
+and a.APNT_CNCL_YN = 'N'
+order by 6
